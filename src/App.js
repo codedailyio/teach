3e4dd2a687ed80387095266a7a02a12c756f5fac
Jason Brown <browniefed@gmail.com>
2018-09-24 21:57:23 -0700

    Final code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
+import produce from "immer";
+
+const counter = produce((draft, props) => {
+  draft.count.counter += props.increaseCount;
+});
 
 class App extends Component {
+  state = {
+    count: {
+      counter: 0,
+    },
+  };
+  componentDidMount() {
+    setInterval(() => {
+      this.setState(counter);
+    }, 1000);
+  }
+
   render() {
-    return <div className="App" />;
+    return <div className="App">{this.state.count.counter}</div>;
   }
 }
 
