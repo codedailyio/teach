30fc3a56652a01ba65df7f6920000db885f28a4d
Jason Brown <browniefed@gmail.com>
2018-09-24 22:29:23 -0700

    Final code

--- src/App.js
+++ src/App.js
@@ -2,20 +2,30 @@ import React, { Component } from "react";
 import produce from "immer";
 
 const counter = produce((draft, props) => {
-  draft.count.counter += props.increaseCount;
-});
+  draft.count.counter += props.increaseCount
+})
 
 class App extends Component {
   state = {
     count: {
       counter: 0,
     },
+    user: {
+      name: "Jason Brown"
+    }
   };
+
   componentDidMount() {
     setInterval(() => {
       this.setState(counter);
-    }, 1000);
+    },1000)
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log(this.state.user === prevState.user)
+  }
+  
+  
 
   render() {
     return <div className="App">{this.state.count.counter}</div>;
