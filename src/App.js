554cb3fa0fa97de50a8eb5eb67dcf700a5332f7a
Jason Brown <browniefed@gmail.com>
2018-11-18 18:15:50 -0800

    Add logs

--- src/App.js
+++ src/App.js
@@ -10,16 +10,21 @@ const logMousePosition = e => {
 const Position = () => {
   useEffect(() => {
     window.addEventListener("mousemove", logMousePosition);
-
-    return () => window.removeEventListener("mousemove", logMousePosition);
-  }, []);
+    console.log("Mounted");
+    return () => {
+      console.log("Unmounted");
+      window.removeEventListener("mousemove", logMousePosition);
+    }
+  });
   return null;
 };
 
 const App = () => {
+  const [trigger, setTrigger] = useState(true);
   const [mounted, setMount] = useState(true);
   return (
     <div>
+      <button onClick={() => setTrigger(!trigger)}>Trigger Update</button>
       <button onClick={() => setMount(!mounted)}>Toggle Mount</button>
       {mounted ? <Position /> : null}
     </div>
