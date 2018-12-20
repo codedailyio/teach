4bbd0b69f3080a9227b4fc296c175cef275a3302
Jason Brown <browniefed@gmail.com>
2018-12-20 15:06:11 -0800

    Outside click code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
 import React, { Component } from "react";
+import "./app.css";
 
 class App extends Component {
+  dropdown = React.createRef();
+  state = {
+    open: false,
+  };
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClickOutside);
+  }
+  handleClickOutside = event => {
+    if (this.dropdown.current && !this.dropdown.current.contains(event.target)) {
+      this.setState({
+        open: false,
+      });
+    }
+  };
+  handleButtonClick = () => {
+    this.setState(state => {
+      return {
+        open: !state.open,
+      };
+    });
+  };
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <div className="container" ref={this.dropdown}>
+          <button type="button" class="button" onClick={this.handleButtonClick}>
+            ☰
+          </button>
+          {this.state.open && (
+            <div class="dropdown">
+              <ul>
+                <li>Option 1</li>
+                <li>Option 2</li>
+                <li>Option 3</li>
+                <li>Option 4</li>
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+    );
   }
 }
 
