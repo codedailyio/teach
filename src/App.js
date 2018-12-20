3adabdec36ed6eb96721090f4cf818e654a0f765
Jason Brown <browniefed@gmail.com>
2018-12-20 15:43:12 -0800

    Rename

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./app.css";
 
 class App extends Component {
-  dropdown = React.createRef();
+  container = React.createRef();
   state = {
     open: false,
   };
@@ -13,7 +13,7 @@ class App extends Component {
     document.removeEventListener("mousedown", this.handleClickOutside);
   }
   handleClickOutside = event => {
-    if (this.dropdown.current && !this.dropdown.current.contains(event.target)) {
+    if (this.container.current && !this.container.current.contains(event.target)) {
       this.setState({
         open: false,
       });
@@ -29,12 +29,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="container" ref={this.dropdown}>
+        <div className="container" ref={this.container}>
           <button type="button" class="button" onClick={this.handleButtonClick}>
             ☰
           </button>
           {this.state.open && (
-            <div class="dropdown">
+            <div class="container">
               <ul>
                 <li>Option 1</li>
                 <li>Option 2</li>
