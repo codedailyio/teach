5e57f0b36925e5e48b1040996d0ddf50a2d60554
Jason Brown <browniefed@gmail.com>
2018-12-29 15:06:52 -0800

    Add in code input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,77 @@
 import React, { Component } from "react";
+import "./input.css";
+
+const CODE_LENGTH = new Array(6).fill(0);
 
 class App extends Component {
+  inputRefs = CODE_LENGTH.map(() => React.createRef());
+  state = {
+    value: "",
+  };
+  handleKeyUp = (e, index) => {
+    e.persist();
+    if (e.key === "Backspace") {
+      if (this.state.value.length !== 0) {
+        this.inputRefs[this.state.value.length - 1].current.focus();
+      }
+    }
+  };
+  handleChange = (e, index) => {
+    let inputValue = e.target.value.slice(-1);
+
+    this.setState(
+      state => {
+        let valueLength = state.value.length - 1;
+        let value = state.value;
+
+        let newValue = inputValue === "" ? value.slice(0, valueLength) : value + inputValue;
+
+        if (index - 1 !== valueLength) {
+          newValue = value.slice(0, index) + inputValue + value.slice(index, valueLength);
+        }
+
+        return {
+          value: newValue,
+        };
+      },
+      () => {
+        if (index !== CODE_LENGTH.length - 1) {
+          this.inputRefs[this.state.value.length].current.focus();
+        }
+      },
+    );
+  };
+  handleFocus = index => {
+    if (this.state.value.length - 1 !== index) {
+      let focusIndex =
+        this.state.value.length !== CODE_LENGTH.length
+          ? this.state.value.length
+          : CODE_LENGTH.length - 1;
+
+      this.inputRefs[focusIndex].current.focus();
+    }
+  };
   render() {
-    return <div className="App" />;
+    const values = this.state.value.split("");
+
+    return (
+      <div className="App">
+        <div className="wrap">
+          {CODE_LENGTH.map((v, index) => {
+            return (
+              <input
+                className="input"
+                value={values[index]}
+                ref={this.inputRefs[index]}
+                onKeyUp={e => this.handleKeyUp(e, index)}
+                onChange={e => this.handleChange(e, index)}
+                onFocus={() => this.handleFocus(index)}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
   }
 }
 
