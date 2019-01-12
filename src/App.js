1591e721ce0c6ebf8932f8a44d9bb54313926149
Jason Brown <browniefed@gmail.com>
2019-01-12 09:51:45 -0800

    Add unread

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import React, { Component } from "react";
+import React, { Component, createRef } from "react";
+import "./app.css";
 
 class App extends Component {
+  state = {
+    value: "",
+    hide: false,
+    focus: false,
+  };
+  handleAutoFill = e => {
+    this.setState({
+      hide: e.animationName === "onAutoFillStart",
+    });
+  };
+
   render() {
-    return <div className="App" />;
+    const { hide, focus, value } = this.state;
+    const hideLabel = hide || focus || value;
+
+    return (
+      <div className="App">
+        <label className="input_container">
+          <span className={`label ${hideLabel ? "hide" : ""}`}>Name</span>
+          <input
+            className="input"
+            value={value}
+            name="email"
+            onAnimationStart={this.handleAutoFill}
+            onFocus={() => this.setState({ focus: true })}
+            onBlur={() => this.setState({ focus: false })}
+            onChange={e => this.setState({ value: e.target.value })}
+          />
+        </label>
+      </div>
+    );
   }
 }
 
