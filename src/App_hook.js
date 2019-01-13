f62cacc7f61d3834be27e0823e03c4a78e2497d7
Jason Brown <browniefed@gmail.com>
2019-01-13 12:10:34 -0800

    Add hook example

---
+++ src/App_hook.js
@@ -0,0 +1,41 @@
+import React, { Component, useState, useRef, useEffect } from "react";
+import "./app.css";
+
+const items = [0, 1, 2, 3, 4, 5, 6, 7];
+
+const App = () => {
+  const [index, setIndex] = useState(0);
+  const increment = useRef(1);
+  useEffect(() => {
+    let interval = setInterval(() => {
+      if (index >= items.length - 1) {
+        increment.current = -1;
+      } else if (index <= 0) {
+        increment.current = 1;
+      }
+
+      setIndex(index + increment.current);
+    }, 500);
+    return () => clearInterval(interval);
+  });
+  return (
+    <div className="App">
+      <div className="background">
+        {items.map(i => {
+          return (
+            <div className="circle">
+              <div
+                className="mover"
+                style={{
+                  transform: `translateX(${(index - i) * 40}px)`,
+                }}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
