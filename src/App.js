7e7e66cf1b2f073211bf017deaf641b051202fac
Jason Brown <browniefed@gmail.com>
2019-01-12 20:44:15 -0800

    Hide

--- src/App.js
+++ src/App.js
@@ -25,7 +25,9 @@ class App extends Component {
             transform: `translateX(${
               !this.state.animate ? "0px" : circleCount * 200
             }px) translateY(-50%)`,
-            transition: `transform ${(circleCount) * 0.75}s linear`,
+            transition: `transform ${circleCount * 0.75}s linear, opacity 0s linear ${circleCount *
+              0.75}s`,
+            opacity: this.state.animate ? 0 : 1,
           }}
         />
         <div className="container">
